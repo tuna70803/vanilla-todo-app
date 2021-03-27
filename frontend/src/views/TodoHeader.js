d586d2af4a5888d3ce58f81206a32f913e04b595
Tuna <tuna70803@gmail.com>
2021-03-27 11:44:27 +0900

    [FE] Todo 헤더에 컨트롤 아이콘 추가
    Todo Header에 폴더 편집, 완료한 아이템 삭제 아이콘 버튼을 추가한다.
    폴더가 중요함 폴더라면 폴더 편집은 표시하지 않는다.
    아직 동작은 구현하지 않았고 이미지와 엘리먼트만 추가했다.

--- frontend/src/views/TodoHeader.js
+++ frontend/src/views/TodoHeader.js
@@ -16,13 +16,8 @@ const TodoHeader = () => {
     const [folderStore, , subscribe] = useFolder();
     subscribe(() => {
         const currentId = folderStore.current;
-        if (currentId === 'important') {
-            nameEl.textContent = '⭐ 중요한 일';
-            return;
-        }
-
-        const folder = folderStore.folders.find(item => item.id === currentId);
-        nameEl.textContent = folder?.name || '';
+        setFolderName(currentId);
+        setEditable(currentId);
     });
 
     /**
@@ -31,6 +26,54 @@ const TodoHeader = () => {
     const nameEl = document.createElement('span');
     nameEl.className = 'todo-header__folder-name';
     el.appendChild(nameEl);
+
+    /**
+     * 폴더 이름을 설정한다.
+     * @param {string} folderId - 폴더 id
+     */
+    const setFolderName = (folderId) => {
+        if (folderId === 'important') {
+            nameEl.textContent = '⭐ 중요한 일';
+            return;
+        }
+
+        const folder = folderStore.folders.find(item => item.id === folderId);
+        nameEl.textContent = folder?.name || '';
+    };
+
+    /**
+     * 편집 아이콘 엘리먼트
+     */
+    const editEl = document.createElement('input');
+    editEl.className = 'todo-header__folder-edit';
+    editEl.type = 'image';
+    editEl.src = 'src/assets/images/ic-edit.svg';
+    editEl.title = '폴더 편집';
+    el.appendChild(editEl);
+
+    /**
+     * 폴더에 맞는 편집 가능 상태를 설정한다.  
+     * 폴더가 important라면 편집 아이콘을 숨겨서 편집할 수 없게 만든다.
+     * @param {string} folderId - 현재 폴더 id
+     */
+    const setEditable = (folderId) => {
+        if (folderId === 'important') {
+            editEl.style.display = 'none';
+            return;
+        }
+
+        editEl.style.display = 'block';
+    };
+
+    /**
+     * 완료한 Todo 지우기 아이콘 엘리먼트
+     */
+    const sweepEl = document.createElement('input');
+    sweepEl.className = 'todo-header__sweep';
+    sweepEl.type = 'image';
+    sweepEl.src = 'src/assets/images/ic-delete-sweep.svg';
+    sweepEl.title = '완료한 항목 지우기';
+    el.appendChild(sweepEl);
 
     return {
         el,
